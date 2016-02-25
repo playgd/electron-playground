f50e0f119ef642e278498c3d45d54e1ee7aa41ba
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-24 22:41:43 -0300

    Add navigation

--- hello-world/client.js
+++ hello-world/client.js
@@ -5,26 +5,28 @@
   const currentWindow = electron.remote.getCurrentWindow()
 
   console.log('test')
-  const $ = (el) => document.querySelector(el);
+  const $ = (el) => document.querySelector(el)
   const on = (el, method, callback) => {
-    el.addEventListener(method, callback, false);
+    el.addEventListener(method, callback, false)
   }
 
   console.log('dom ready!')
   const $minimize = $('[data-js="minimize"]')
   const $maximize = $('[data-js="maximize"]')
   const $close = $('[data-js="close"]')
+  const $prev = $('[data-js="prev"]')
+  const $next = $('[data-js="next"]')
 
   $maximize.shouldMaximize = true
 
   on($minimize, 'click', () => currentWindow.minimize())
+
   on($maximize, 'click', () => {
     console.log('Should maximize?', $maximize.shouldMaximize)
     if($maximize.shouldMaximize) {
       $maximize.shouldMaximize = false
       return currentWindow.maximize()
     }
-
     $maximize.shouldMaximize = true
     currentWindow.restore()
   })
@@ -32,5 +34,15 @@
   on($close, 'click', () => {
     console.log('click')
     currentWindow.close()
+  })
+
+  on($next, 'click', () => {
+    console.log('next')
+    window.history.forward()
+  })
+
+  on($prev, 'click', () => {
+    console.log('prev')
+    window.history.back()
   })
 })()
